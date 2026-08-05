dd33e31359e2d39af1d12da350cce9777412aea6
sterlinr08-dte <sterlinr08@gmail.com>
2026-08-04 21:37:06 -0400

    feat(frontend): integrar proveedor de empresa activa

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AuthProvider } from './lib/auth'
+import { EmpresaProvider } from './lib/empresa'
 import { NegocioProvider } from './lib/negocio'
 import './index.css'
 
@@ -10,9 +11,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <NegocioProvider>
-          <App />
-        </NegocioProvider>
+        <EmpresaProvider>
+          <NegocioProvider>
+            <App />
+          </NegocioProvider>
+        </EmpresaProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
